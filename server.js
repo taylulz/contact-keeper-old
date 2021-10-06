c6d1d5cb8e737d3c395c008025cc90f4281a0dff
taylor d <taylor.delph@eyecuelab.com>
2021-10-06 13:17:49 -0700

    convert response to json object

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.json({ msg: 'welcome to contact-keeper api'}));
 
 const PORT = process.env.PORT || 5000;
 
