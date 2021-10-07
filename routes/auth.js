d5c3c524e60a914b6e25099dcf204ff580258f00
taylor d <taylor.delph@eyecuelab.com>
2021-10-07 08:14:52 -0700

    update auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -17,8 +17,56 @@ router.get('/', (req, res) => {
 // @route   POST api/auth
 // @desc    Auth user & get token
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Log in user')
-}); 
+router.post(
+  '/', 
+  [
+    check('email', 'Please include valid email').isEmail(),
+    check('password', 'Password is reqired').exists()
+  ], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-module.exports = router;
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if(!user){
+        return res.status(400).json({ msg: 'Invalid Email' })
+      }
+      
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if(!isMatch){
+        return res.status(400).json({ msg: 'invalid Password' });
+      }
+      
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload, 
+        config.get('jwtSecret'), 
+        {
+          expiresIn: 36000
+        }, 
+        (err, token) => {
+          if(err) throw err;
+          res.json({ token })
+        }
+      ); 
+       
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send('Server Error')
+    }
+  }
+); 
+
+module.exports = router;  
