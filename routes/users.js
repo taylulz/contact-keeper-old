edc2ff10286dc42855b3b78fd7b8794222bc8494
taylor d <taylor.delph@eyecuelab.com>
2021-10-07 07:09:11 -0700

    hash passwords and register route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check')
 
 const User = require('../models/User');
@@ -18,13 +19,42 @@ router.post(
     check('password', 'Please enter password with 6+ characters')
       .isLength({ min: 6 })
   ], 
-  (req, res) => {
-   const errors = validationResult(req);
-   if(!errors.isEmpty()) {
-     return res.status(400).json({ errors: errors.array() });
-   }
+  async (req, res) => {
+    
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-   res.send('passed');
+    const { name, email, password } = req.body;
+    
+
+    try {
+      let user = await User.findOne({ email });
+
+      if(user) {
+        return res.status(400).json({ msg: 'user already exists'});
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+      
+      // before saving we must encrypt
+      const salt = await bcrypt.genSalt(10);
+      
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("User saved test");
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
