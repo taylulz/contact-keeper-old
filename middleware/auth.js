78be28240d20d7434bac062a455d9bf9d1ab01ee
taylor d <taylor.delph@eyecuelab.com>
2021-10-07 14:14:09 -0700

    add auth middleware & protected routes

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+// middleware is just a function that has access to the request and response cycle and object. Any time we hit endpoints we can fire off this middleware
+
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+
+module.exports = function(req, res, next) {
+  // get  token from header
+  const token = req.header('x-auth-token');
+
+  // check if not token
+  if(!token) {
+    return res.status(401).json({ msg: 'no token, auth denied' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: 'token is not valid' })
+  }
+}
